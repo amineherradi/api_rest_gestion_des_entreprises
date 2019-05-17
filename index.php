4642a2852e4ef3564364b31d8986551354b99013
Amine Herradi <amineherradi@gmail.com>
2019-05-17 18:49:55 +0200

    Feature - #34 - PSR for index.php

--- index.php
+++ index.php
@@ -2,84 +2,47 @@
 
 require_once './config/config.php';
 foreach ($entities as $key => $entity) {
-	require_once './controllers/'.$entity.'Controller.php';
+    require_once './controllers/'.$entity.'Controller.php';
 }
 
-if(
-	isset($_GET['entity']) && 
-	in_array($_GET['entity'], $entities)
-) {
-	$entityController = $_GET['entity'].'Controller';
-	$entityController = new $entityController;
+if (isset($_GET['entity']) && in_array($_GET['entity'], $entities)) {
+    $entityController = $_GET['entity'].'Controller';
+    $entityController = new $entityController;
 }
 
 switch ($_SERVER['REQUEST_METHOD']) {
-	case 'GET':
-		if (
-			(
-				isset($_GET['action']) && 
-				isset($_GET['id'])
-			) && (
-				$_GET['action'] == 'detail' &&
-				$_GET['id'] != ''
-			)
-		) {
-			$entityController->getDetail_request($_GET['id']);
-		} elseif (
-			(
-				isset($_GET['action']) && 
-				!isset($_GET['id'])
-			) && $_GET['action'] == 'all'
-		) {
-			$entityController->getAll_request();
-		}
-		break;
+    case 'GET':
+        if ((isset($_GET['action']) && isset($_GET['id'])) && ($_GET['action'] == 'detail' && $_GET['id'] != '')) {
+            $entityController->getDetail_request($_GET['id']);
+        } elseif ((isset($_GET['action']) && !isset($_GET['id'])) && $_GET['action'] == 'all') {
+            $entityController->getAll_request();
+        }
+        break;
 
-	case 'POST':
-		if (
-			(
-				isset($_GET['action']) && 
-				!isset($_GET['id'])
-			) && $_GET['action'] == 'add'
-		) {
-			$entityController->add_request($_POST);
-		}
-		break;
+    case 'POST':
+        if ((isset($_GET['action']) && !isset($_GET['id'])) && $_GET['action'] == 'add') {
+            $entityController->add_request($_POST);
+        }
+        break;
 
-	case 'PATCH':
-		if (
-			(
-				isset($_GET['action']) && 
-				isset($_GET['id'])
-			) && (
-				$_GET['action'] == 'update' &&
-				$_GET['id'] != ''
-			)
-		) {
-			$patch = file_get_contents('php://input');
-			$patch = urldecode($patch);
-			$patch = explode("&", $patch); 
+    case 'PATCH':
+        if ((isset($_GET['action']) && isset($_GET['id'])) && ($_GET['action'] == 'update' && $_GET['id'] != '')) {
+            $patch = file_get_contents('php://input');
+            $patch = urldecode($patch);
+            $patch = explode("&", $patch); 
 
-			foreach ($patch as $key => $value) {
-				$value = explode("=", $value);
-				$_PATCH[$value[0]] = $value[1]; 
-			}
+            foreach ($patch as $key => $value) {
+                $value = explode("=", $value);
+                $_PATCH[$value[0]] = $value[1]; 
+            }
 
-			$entityController->update_request($_GET['id'], $_PATCH);
-		}
-		break;
+            $entityController->update_request($_GET['id'], $_PATCH);
+        }
+        break;
 
-	case 'DELETE':
-		if (
-			(
-				isset($_GET['action']) && 
-				isset($_GET['id'])
-			) && (
-				$_GET['action'] == 'delete' &&
-				$_GET['id'] != ''
-			)
-		) {
-			$entityController->delete_request($_GET['id']);
-		}
-		break;
+    case 'DELETE':
+        if ((isset($_GET['action']) && isset($_GET['id'])) && ($_GET['action'] == 'delete' && $_GET['id'] != '')) {
+            $entityController->delete_request($_GET['id']);
+        }
+        break;
 }
